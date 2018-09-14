09d21c3df7519d4bac5213422e91ec6ae1af7305
Bian, Maosong <msbian@telenav.cn>
2018-09-14 17:29:27 +0800

    update ensemble learning demo

--- ensemble_learning.py
+++ ensemble_learning.py
@@ -12,13 +12,15 @@ from sklearn.ensemble import VotingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
 ''' 投票分类'''
 X, y = make_moons(n_samples=500, noise=0.3)
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 log_clf = LogisticRegression(random_state=42)
-rnd_clf =  RandomForestClassifier(random_state=42)
+rnd_clf = RandomForestClassifier(random_state=42)
 svm_clf = SVC(random_state=42)
 
 voting_clf = VotingClassifier(estimators=[
@@ -43,8 +45,43 @@ y_pred = bag_clf.predict(X_test)
 print('\n bagging: ', accuracy_score(y_test, y_pred))
 
 
+'''random forests'''
+rnd_clf = RandomForestClassifier(n_estimators=500, max_leaf_nodes=16, n_jobs=-1, random_state=42)
+rnd_clf.fit(X_train, y_train)
+y_pres_rf = rnd_clf.predict(X_test)
+
+print('\n random forests: ', accuracy_score(y_test, y_pres_rf))
 
 
+np.random.seed(42)
+X = np.random.rand(100, 1) - 0.5
+y = 3*X[:, 0]**2 + 0.05 * np.random.randn(100)
+X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=49)
+
+'''GradientBoosting'''
+from sklearn.ensemble import GradientBoostingRegressor
+gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=3, random_state=42)
+gbrt.fit(X_train, y_train)
+
+
+'''early stopping'''
+gbrt = GradientBoostingRegressor(max_depth=2, warm_start=True, random_state=42)
+min_val_error = float('inf')
+error_going_up = 0
+for n_nestimators in range(1, 120):
+     gbrt.n_estimators = n_nestimators
+     gbrt.fit(X_train, y_train)
+     y_pred = gbrt.predict(X_val)
+     val_error = mean_squared_error(y_val, y_pred)
+     if val_error < min_val_error:
+         min_val_error = val_error
+         error_going_up = 0
+     else:
+         error_going_up += 1
+         if error_going_up == 5:
+             break
+print('\nearly stopping: gbrt.n_estimators:', gbrt.n_estimators)
+print('MSE:', min_val_error)
 
 
 
