1915f69913fc0abbb0d453fcba2570d1117ab5eb
Bian, Maosong <msbian@telenav.cn>
2018-09-16 22:03:12 +0800

    add 21 projects of tensorflow

---
+++ 21PorjectsOfTensorflow/1_MNIST.py
@@ -0,0 +1,112 @@
+# -*- coding: UTF-8 -*-
+"""
+Date: 2018-9-16
+Note: MNIST to learn ML
+Ref:  《21个项目玩转深度学习 --基于TensorFlow的实战详解》
+    第 1 章 MNIST机器学习入门
+"""
+
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+
+mnist = input_data.read_data_sets('MNIST_data/', one_hot=True, source_url='http://yann.lecun.com/exdb/mnist/')
+
+print(mnist.train.images.shape)
+print(mnist.train.labels.shape)
+# print(mnist.train.images[0, :])
+
+def basic_linear_model():
+    learning_rate = 0.01
+    X = tf.placeholder(tf.float32, [None, 784])
+    W = tf.Variable(tf.zeros([784, 10]))
+    b = tf.Variable(tf.zeros([10]))
+    y = tf.nn.softmax(tf.matmul(X, W) + b)
+    y_ = tf.placeholder(tf.float32, [None, 10])
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y)))
+    train_step = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
+
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
+
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    batch_size = 100
+    for i in range(1000):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size=batch_size)
+        sess.run(train_step, feed_dict={X: batch_xs, y_: batch_ys})
+
+    print(sess.run(accuracy, feed_dict={X: batch_xs, y_: batch_ys}))
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+def cnn_mnist():
+    X = tf.placeholder(tf.float32, [None, 784])
+    y_ = tf.placeholder(tf.float32, [None, 10])
+    x_image = tf.reshape(X, [-1, 28, 28, 1])
+
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
+    for i in range(500):
+        batch = mnist.train.next_batch(50)
+        train_step.run(feed_dict={X: batch[0], y_: batch[1], keep_prob: 0.5})
+        if i % 30 == 0:
+            train_accuracy = accuracy.eval(feed_dict={X: batch[0], y_: batch[1], keep_prob: 1.0})
+            print('Step %d, training accuracy:%f' % (i, train_accuracy))
+    print('test accuracy %f' % accuracy.eval(feed_dict={X: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+
+if __name__ == '__main__':
+    # basic_linear_model()
+    cnn_mnist()
+
+
+
+
+
+
+
